076adf8e72b3ae7ff2329bcde6c309696ca22103
VladyslavKurdijashko <159957674+VladyslavKurdijashko@users.noreply.github.com>
2024-04-23 22:20:10 +0300

    upd index

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
 
 
 function getElementWidth(content, padding, border) {
+    
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding);
     const borderWidth = parseFloat(border);
